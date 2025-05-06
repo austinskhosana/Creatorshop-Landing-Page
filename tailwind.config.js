011a8985328c16fb6b68cb303f4e229a1ac53da2
Austin Skhosana <austinskhosana@icloud.com>
2025-05-06 21:56:45 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        limegreen: '#A3FF38',
+        lime: "#7AE200"
+      },
+    },
+  },
+  plugins: [],
+} 
